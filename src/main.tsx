484aa8c8af9664a9c80c5190de19d7c08798bc30
Ermiyas9 <115026605+Ermiyas9@users.noreply.github.com>
2026-03-01 01:26:41 -0500

    Move project to repo root for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/frontend-assignment-Ermiyas9/">
       <MantineProvider>
         <App />
       </MantineProvider>
